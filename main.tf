eae4c33a1a10e700a4aaa938f640e39698540aac
root <you@example.com>
2026-08-18 17:03:18 +0000

    add 2rg

---
+++ main.tf
@@ -0,0 +1,8 @@
+resource "azurerm_resource_group" "rg1" {
+  name     = "example1"
+  location = "West Europe"
+}
+resource "azurerm_resource_group" "rg2" {
+  name     = "example2"
+  location = "West Europe"
+}
